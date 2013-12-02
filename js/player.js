1e0ca484dd15092cd785cddd05e6e4c3649822f7
MissFilly <lady.filly@gmail.com>
2013-12-01 22:51:15 -0300

    Cat sprites

--- js/player.js
+++ js/player.js
@@ -1,10 +1,25 @@
 function newPlayer() {
-	var square = new Graphics().beginFill("#F00").drawRect(0, 0, 22, 22);
-	var player = new Shape(square);
+	/*var square = new Graphics().beginFill("#F00").drawRect(0, 0, 22, 22);
+	 var player = new Shape(square);*/
+	var data = {
+		images : ["img/cat.png"],
+		frames : {
+			width : 42,
+			height : 42
+		},
+		animations : {
+			run : {
+				frames: [0,1,2],
+				speed: 0.1
+			}
+		}
+	};
+	var spriteSheet = new SpriteSheet(data);
+	var player = new Sprite(spriteSheet, "run");
 	player.x = 150;
 	player.y = 50;
-	player.w = 22;
-	player.h = 22;
+	player.w = 42;
+	player.h = 42;
 	player.yVel = 0;
 	player.onGround = false;
 
@@ -26,14 +41,14 @@ function playerCollision() {
 		var colUp;
 		var colDown;
 
-		colDown = pointCollision(player.x + 11, player.y + 22, platform);
-		colUp = pointCollision(player.x + 11, player.y - 1, platform);
-		colRight = pointCollision(player.x + 23, player.y + 11, platform);
+		colDown = pointCollision(player.x + player.w/2, player.y + player.h, platform);
+		colUp = pointCollision(player.x + player.w/2, player.y - 1, platform);
+		colRight = pointCollision(player.x + player.w/2+1, player.y + player.h/2, platform);
 
 		if (colDown) {
 			player.onGround = true;
 			player.yVel = 0;
-			player.y = platform.y - 22;
+			player.y = platform.y - player.h;
 		}
 		if (colUp) {
 
@@ -46,7 +61,7 @@ function playerCollision() {
 
 	}
 
-	for (var i=0; i<birds.length; i++) {
+	for (var i = 0; i < birds.length; i++) {
 		var bird = birds[i];
 		if (player.x < bird.x + bird.w && player.x + player.w > bird.x && player.y < bird.y + bird.h && player.y + player.h > bird.y) {
 			stage.removeChild(bird);
